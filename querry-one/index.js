fa54d41f961f1c15e8853cdf5e7d1773ff8d39a2
Immaculate <chepkiruiimmaculatekogo@gmail.com>
2025-05-20 16:13:15 +0300

    made changes

---
+++ querry-one/index.js
@@ -0,0 +1,18 @@
+function uniqueItems(arr) {
+    const seen = new Set();
+    const duplicates = new Set();
+
+for (const item of arr) {
+    if (seen.has(item)) {
+      duplicates.add(item);
+    } else {
+      seen.add(item);
+        }
+    }
+
+    return Array.from(seen).filter(item => !duplicates.has(item)).sort((a,b) => a-b);
+}
+
+
+const arr = [1,2,3,4,1,213,78,89,81,23,45,5,6,12,34,56,76,54,12];
+console.log(uniqueItems(arr)); 
