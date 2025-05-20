57c6cc14b1029be1086d7010f23bd498fb7587d6
Immaculate <chepkiruiimmaculatekogo@gmail.com>
2025-05-20 15:19:30 +0300

    Removed duplicates in a list and sorted the unique items

---
+++ question-one/index.js
@@ -0,0 +1,22 @@
+// Design the program that processes a list of items and identifies items that appear
+//  uniquely within it. Consider how you might wantto organize and check
+//  for uniqueness
+
+function findUniqueItems(arr) {
+    const seen = new Set();
+    const duplicates = new Set();
+
+for (const item of arr) {
+    if (seen.has(item)) {
+      duplicates.add(item);
+    } else {
+      seen.add(item);
+        }
+    }
+
+    return Array.from(seen).filter(item => !duplicates.has(item)).sort((a,b) => a-b);
+}
+
+
+const arr = [1,2,3,4,1,213,78,89,81,23,45,5,6,12,34,56,76,54,12];
+console.log(findUniqueItems(arr)); 
